626c8a4654a7f4490086e4eaa50395120639b095
nshcn <2695563464@qq.com>
2022-02-26 00:30:14 +0800

    引入codemirror

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
+    <test :code="code"></test>
     <edit></edit>
   </div>
 </template>
 
 <script>
 import Edit from '@/components/Edit.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
     Edit,
+    Test,
   },
   watch: {},
   data() {
-    return {}
+    return {
+      code: 'fdsfjdk',
+    }
   },
   methods: {},
 }
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueCodemirror from 'vue-codemirror'
+// import base style
+import 'codemirror/lib/codemirror.css'
+// import more codemirror resource...
 
+Vue.use(VueCodemirror, /* {
+  options: { theme: 'base16-dark', ... },
+  events: ['scroll', ...]
+} */)
 
 Vue.config.productionTip = false
 
